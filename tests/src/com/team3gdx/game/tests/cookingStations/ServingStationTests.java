e1f2e7600651f58b2958646d0b86264d24b7a8a1
IsaacR89 <imrhodes8975@gmail.com>
2023-05-02 16:39:19 +0100

    station testing and tutorial button

---
+++ tests/src/com/team3gdx/game/tests/cookingStations/ServingStationTests.java
@@ -0,0 +1,11 @@
+package com.team3gdx.game.tests.cookingStations;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(GdxTestRunner.class)
+public class ServingStationTests {
+    @Test
+    public void testServeCustomer (){
+
+    }
+}
